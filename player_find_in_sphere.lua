1fe86cc978e508fe77a66e931b9496e7179ba264
Oliver <starlight.dev@hotmail.com>
2021-07-31 07:28:05 +0100

    Update player_find_in_sphere.lua

--- player_find_in_sphere.lua
+++ player_find_in_sphere.lua
@@ -1,7 +1,7 @@
 function player.FindInSphere(pos, dist)
     dist = dist * dist
     local t = {}
-    for index, ply in ipairs(player.GetAll()) do
+    for _, ply in ipairs(player.GetAll()) do
         if ply:GetPos():DistToSqr(pos) < dist then
             t[#t + 1] = ply
         end
