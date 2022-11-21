00e5a02a4600bcfe8c21f3ef5b21cfaf56996424
Oliver <starlight.dev@hotmail.com>
2022-11-21 21:32:55 +0000

    Update player_find_in.lua
    - Returned count

--- player_find_in.lua
+++ player_find_in.lua
@@ -1,22 +1,28 @@
 function player.FindInSphere(pos, dist)
-    dist = dist * dist
-    local t = {}
-    for _, ply in ipairs(player.GetAll()) do
-        if ply:GetPos():DistToSqr(pos) < dist then
-            t[#t + 1] = ply
-        end
-    end
-    return t
+	dist = dist * dist
+	local t = {}
+	local l = 0
+
+
+	for _, ply in ipairs(player.GetAll()) do
+		if ply:GetPos():DistToSqr(pos) < dist then
+			l = l + 1
+			t[l] = ply
+		end
+	end
+	return t, l
 end
 
 function player.FindInBox(mins, maxs)
 	mins, max = OrderVectors(mins, maxs)
 	local players = {}
+	local count = 0
 	for index, ply in ipairs(player.GetAll()) do
 		local pos = ply:GetPos()
 		if pos[1] >= mins[1] and pos[1] <= maxs[1] and pos[2] >= mins[2] and pos[2] <= maxs[2] and pos[3] >= mins[3] and pos[3] <= maxs[3] then
-			players[#players + 1] = ply
+			count = count + 1
+			players[count] = ply
 		end
 	end
-	return players
+	return players, count
 end
